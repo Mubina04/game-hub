dd6034dadbb740bb7bbf66815afc767db8d99d43
Mubinabegim <mubinabegimxayrullayeva@gmail.com>
2024-05-30 11:35:06 +0500

    Create responsive layout and add colorSwitchMode

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
-
+import NavBar from "./components/NavBar";
 function App() {
-  return <></>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        <GridItem bg={"royalblue"} area={"aside"}>
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem bg={"red"} area={"main"}>
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
